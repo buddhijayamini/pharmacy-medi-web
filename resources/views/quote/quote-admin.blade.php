6e236e6e4357097aa97fd306a672db2399afd749
Buddhini SSE <buddhigeethen@gmail.com>
2024-05-15 20:08:15 +0530

    changes

--- resources/views/quote/quote-admin.blade.php
+++ resources/views/quote/quote-admin.blade.php
@@ -1,6 +1,4 @@
 @extends('layouts.app')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 @section('content')
     <div class="container mt-3">
